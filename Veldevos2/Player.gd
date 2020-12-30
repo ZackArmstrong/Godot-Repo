e031ba389d5b17b3ff9f1ce020d6e81191b83fa7
ZackArmstrong <66346877+ZackArmstrong@users.noreply.github.com>
2020-12-30 18:11:08 -0500

    Removed State Machine - Temp Attack

--- Veldevos2/Player.gd
+++ Veldevos2/Player.gd
@@ -2,13 +2,9 @@ extends KinematicBody2D
 
 var motion = Vector2()
 var leftOrRight = 1
-var state = MOVE
+var isAttacking = false
 
-enum{
-	MOVE,
-	ATTACK,
-	ROLL
-}
+
 
 const UP = Vector2(0, -1)
 const GRAVITY = 20
@@ -18,26 +14,15 @@ const ACCELERATION = 100
 
 onready var animationPlayer = $AnimationPlayer
 
+
 func _physics_process(delta):
-	print(state)
-	match state:
-		MOVE:
-			move_state()
-		ATTACK:
-			attack_state(delta)
-		ROLL:
-			pass
-
-
-func move_state():
-	
 	motion.y += GRAVITY
 	var friction = false
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("ui_right") && isAttacking == false:
 		motion.x = min(motion.x+ACCELERATION, MAX_SPEED)
 		$AnimationPlayer.play("RunRight")
 		leftOrRight = 1
-	elif Input.is_action_pressed("ui_left"):
+	elif Input.is_action_pressed("ui_left") && isAttacking == false:
 		motion.x = max(motion.x-ACCELERATION, -MAX_SPEED)
 		$AnimationPlayer.play("RunLeft")
 		leftOrRight = 0
@@ -45,9 +30,9 @@ func move_state():
 	else: 
 		friction = true
 		motion.x = lerp(motion.x, 0, 0.2)
-		if leftOrRight == 1:
+		if leftOrRight == 1 && isAttacking == false:
 			$AnimationPlayer.play("IdleRight")
-		else: 
+		elif isAttacking == false: 
 			$AnimationPlayer.play("IdleLeft")
 	
 	if is_on_floor():
@@ -56,10 +41,26 @@ func move_state():
 		if friction == true:
 			motion.x = lerp(motion.x, 0, 0.05)
 	
+	if Input.is_action_just_pressed("attack"):
+		if leftOrRight == 1:
+			$AnimationPlayer.play("AttackRight")
+			isAttacking = true
+		elif leftOrRight == 0:
+			$AnimationPlayer.play("AttackLeft")
+			isAttacking = true
+	
 	motion = move_and_slide(motion, UP)
 	
-	if Input.is_action_just_pressed("attack"):
-		state = ATTACK
 
-func attack_state(delta):
-	pass
+
+
+
+
+
+func _on_AnimationPlayer_animation_finished(AttackRight):
+	#print(AttackRight)
+	#if $AnimationPlayer.is_playing() == "AttackRight":
+	#	print("test")
+	isAttacking = false
+	#if $AnimationPlayer.current_animation == "AttackLeft":
+	#	isAttacking = false
